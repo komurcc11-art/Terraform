b6af88b9ccd43f04e36251585e3ef8212dc5f08e
komurcc11-art <komurcc11@gmail.com>
2025-11-12 03:45:59 +0000

    terraform modules

--- output-tf/main.tf
+++ output-tf/main.tf
@@ -14,6 +14,7 @@ resource "docker_image" "nginx" {
 }
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
+  network_mode = "bridge"
   name  = var.container_name
   ports {
     internal = 80
